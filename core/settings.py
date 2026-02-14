24dc11d87ed5076c5385c483a936be13dc178695
WabukoWabuko <basilwabbs@gmail.com>
2026-02-14 06:50:57 +0300

    make some crazy changes to make the app function

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
     'rest_framework',
     'corsheaders',
-    'tasks'
 ]
 
 MIDDLEWARE = [
