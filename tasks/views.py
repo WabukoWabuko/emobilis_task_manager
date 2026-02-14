6cf28133efbf48cd2eebc9323ce322ca2a224310
WabukoWabuko <basilwabbs@gmail.com>
2026-02-14 05:37:07 +0300

    Setting up the V in MVT, the views step

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    # This helps in telling the DRF on which data to retrieve from the database
+    queryset = Task.objects.all().order_by("-created_at")
+
+    # At this point we instruct the serializer on what  to use to parse the data.
+    serializer_id = TaskSerializer
+    serializer_class = TaskSerializer
